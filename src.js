59343e5564edf9877fbbcb237b36716dbb0cc65a
Dat <writephudat@gmail.com>
2022-08-31 04:14:34 +0700

    Add 'KJV'

--- src.js
+++ src.js
@@ -1,7 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('DOMContentLoaded', function () {
+  document
+    .querySelectorAll([`[data-usfm="${this.location.hash}"]`])
+    .forEach((node, index) => {
+      console.log(node);
+      node.scrollIntoView({
+        behavior: 'auto',
+        block: 'center',
+        inline: 'center',
+      });
+      node.style.color = 'blue';
+      setTimeout(() => {
+        node.style.color = 'initial';
+      }, 5000);
+    });
+
   const navActiveElement = document.querySelector(
     `[href*="${window.location.pathname.split('/').pop()}"]`
   );
-  navActiveElement.scrollIntoView();
-  navActiveElement.classList.add('active');
+  navActiveElement &&
+    navActiveElement.scrollIntoView({
+      behavior: 'auto',
+      block: 'center',
+      inline: 'center',
+    });
+  navActiveElement && navActiveElement.classList.add('active');
+
+  document
+    .querySelectorAll('[data-level="verse"][data-usfm]')
+    .forEach((node) => {
+      node.outerHTML = `<a href="../${
+        location.pathname.match('vie1925') ? 'kjv' : 'vie1925'
+      }/${node.dataset.usfm.replace(/[.].*/, '.html')}#${
+        node.dataset.usfm
+      }">${node.outerHTML}</a>`;
+    });
 });
